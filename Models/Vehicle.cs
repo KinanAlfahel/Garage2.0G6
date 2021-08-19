7aeafb34a4223a85defad1e3b1bb516c5c0c7e24
KinanAlfahel <85742709+KinanAlfahel@users.noreply.github.com>
2021-08-19 10:09:51 +0200

    base properties

---
+++ Models/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage2._0G6.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Regnum { get; set; }
+
+        public string Color { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public int Wheel { get; set; }
+
+        public DateTime Arrivaldate { get; set; }
+        
+
+
+    }
+}
