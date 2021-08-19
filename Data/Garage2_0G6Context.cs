1e9976e3bddd4a223bf2d2c00eeabd87066377e5
Christoper <c.blake@hotmail.se>
2021-08-19 11:12:26 +0200

    Added data

---
+++ Data/Garage2_0G6Context.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Garage2._0G6.Models;
+
+namespace Garage2._0G6.Data
+{
+    public class Garage2_0G6Context : DbContext
+    {
+        public Garage2_0G6Context (DbContextOptions<Garage2_0G6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Garage2._0G6.Models.Vehicle> Vehicle { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Vehicle>()
+               .HasData(
+                   new Vehicle { Id = 1, Type = "A", Regnum = "123ABC", Color = "red", Brand = "Volvo", Model = "X3", Wheel = 4, Arrivaldate = DateTime.Now },
+                   new Vehicle { Id = 2, Type = "A", Regnum = "125ABC", Color = "red", Brand = "Volvo", Model = "X3", Wheel = 4, Arrivaldate = DateTime.Now }
+
+               );
+        }
+    }
+}
---
+++ Migrations/20210819090843_Init.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Garage2._0G6.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Regnum = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Brand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Wheel = table.Column<int>(type: "int", nullable: false),
+                    Arrivaldate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicle", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Vehicle",
+                columns: new[] { "Id", "Arrivaldate", "Brand", "Color", "Model", "Regnum", "Type", "Wheel" },
+                values: new object[] { 1, new DateTime(2021, 8, 19, 11, 8, 42, 564, DateTimeKind.Local).AddTicks(4041), "Volvo", "red", "X3", "123ABC", "A", 4 });
+
+            migrationBuilder.InsertData(
+                table: "Vehicle",
+                columns: new[] { "Id", "Arrivaldate", "Brand", "Color", "Model", "Regnum", "Type", "Wheel" },
+                values: new object[] { 2, new DateTime(2021, 8, 19, 11, 8, 42, 567, DateTimeKind.Local).AddTicks(9474), "Volvo", "red", "X3", "125ABC", "A", 4 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicle");
+        }
+    }
+}
