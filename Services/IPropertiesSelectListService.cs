09af0cda65006af164a07457c877ac4bc34e9e9c
Christoper <c.blake@hotmail.se>
2021-08-25 11:20:53 +0200

    InterFace for a functionality that might start to work

---
+++ Services/IPropertiesSelectListService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Garage2._0G6.Services
+{
+    public interface IPropertiesSelectListService
+    {
+        Task<IEnumerable<SelectListItem>> GetColumnsAsync();
+    }
+}
